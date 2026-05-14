658c053a4595176cb1ad2993e9820a69042f79a3
Hmtpanchal <111943353+Hmtpanchal@users.noreply.github.com>
2026-05-14 18:17:24 +0530

    Add AccountRepository interface for account management

---
+++ AccountRepository.java
@@ -0,0 +1,10 @@
+package com.hemant.banking.repository;
+
+import com.hemant.banking.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByAccountNumber(String accountNumber);
+}
